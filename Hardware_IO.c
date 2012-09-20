eddf37d7499e439f5291657f205519aab50f5e02
Lukkio <lucio456@hotmail.it>
2012-09-20 21:10:12 +0300

    Update Hardware_IO.c

--- Hardware_IO.c
+++ Hardware_IO.c
@@ -1,4 +1,3 @@
-
 //lcd Pinout
 #DEFINE LCD_CS PIN_B3
 #DEFINE LCD_SDO PIN_C7
@@ -12,6 +11,9 @@
 #DEFINE PB_RIGHT PIN_A2 
 #DEFINE PB_OK PIN_A5 
 
+///IR
+#DEFINE IR PIN_A0
+
 ///////////////Registri ///////////////////// pagina 66
 
 ///Porta SPI
@@ -20,3 +22,16 @@
 #byte SSPBUF = 0xFC9
 #bit SSPEN = SSPCON1.5
 #bit BF = SSPSTAT.0
+
+//Timer1
+#byte TMR1H = 0xFCF
+#byte TMR1L = 0xFCE
+#byte PIR1 = 0xF9E
+#bit TMR1IF = PIR1.0
+#byte INTCON = 0xFF2
+#byte PIE1 = 0xF9D
+#byte T1CON = 0xFCD
+
+#bit GIE = INTCON.7
+#bit PEIE = INTCON.6
+#bit TMR1IE = PIE1.0
